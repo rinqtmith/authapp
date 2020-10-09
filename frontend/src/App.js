dde88ef952d08115834e401c596d67624b1871f3
Fatih Avcı <rinqtf@gmail.com>
2020-10-09 23:19:36 +0300

    added home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { CssBaseline } from '@material-ui/core';
 
 import Home from './components/home/home';
 
-const App = () => <Home />;
+const App = () => {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </React.Fragment>
+  );
+};
 
 export default App;
--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,30 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { makeStyles } from '@material-ui/core/styles';
-
-import { Card, CardHeader, Container, CssBaseline } from '@material-ui/core';
+import {
+  Card,
+  CardHeader,
+  Container,
+  CssBaseline,
+  Typography,
+  Button,
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     textAlign: 'center',
-    marginTop: '40vh',
+    marginTop: '30vh',
   },
   card: {
     padding: '3rem',
     borderRadius: '2rem',
+  },
+  button: {
+    marginTop: '2rem',
+  },
+  link: {
+    textDecoration: 'none',
   },
 }));
 
 const Home = () => {
   const classes = useStyles();
 
+  const [darkMode, setDarkMode] = useState(false);
+
+  const palleteType = darkMode ? 'dark' : 'light';
+
+  let theme = createMuiTheme({
+    palette: {
+      type: palleteType,
+    },
+    overrides: {
+      MuiAppBar: {
+        colorPrimary: {
+          backgroundColor: darkMode ? '#303030' : '#1976d2',
+        },
+      },
+    },
+  });
+  theme = responsiveFontSizes(theme);
+
   return (
-    <Container maxWidth='sm' className={classes.root}>
-      <CssBaseline />
-      <Card raised variant='outlined' className={classes.card}>
-        <CardHeader title='Go to authentication' />
-      </Card>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='sm' className={classes.root}>
+        <CssBaseline />
+        <Card raised variant='outlined' className={classes.card}>
+          <CardHeader
+            title='Go to authentication'
+            component={Link}
+            to='signup'
+            className={classes.link}
+            style={{ color: darkMode ? 'yellow' : 'blue' }}
+          />
+          <Typography variant='subtitle1'>
+            rinqtf@gmail.com &copy;{new Date().getFullYear()}
+          </Typography>
+          <Button
+            variant='contained'
+            color='primary'
+            className={classes.button}
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            Change theme
+          </Button>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 };
 
